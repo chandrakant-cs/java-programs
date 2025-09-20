d16f7e7185bb36a0b640502afb2c8b94a3678b1d
chandrakant-cs <chandrakant.cs25@bmsce.ac.in>
2025-09-20 11:42:45 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,10 @@
+class second
+{
+    public static void main(String[] args) {
+        int a=100;
+        System.out.println("This is Number:"+a);
+        a=a*2;
+        System.out.println("Number is:");
+        System.out.println(a);
+    }
+}
