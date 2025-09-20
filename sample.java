650e3409db696e83fd24f5df5523ec91d8019d4e
chandrakant-cs <chandrakant.cs25@bmsce.ac.in>
2025-09-20 11:49:30 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,9 @@
+class sample {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        add ();
+    }
+    static void add(){
+        System.out.println("Good Morning");
+    }
+}
